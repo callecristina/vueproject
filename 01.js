0495fc0d30202f9c2e31d3dfc55177c07314e016
Cristina Calle <87840049+callecristina@users.noreply.github.com>
2022-02-15 10:06:48 -0500

    Add files via upload
    Creación de proyecto con Vue

---
+++ 01.js
@@ -0,0 +1,12 @@
+const app=new Vue({
+    el: "#app",
+    data:{
+        titulo: "hola mundo con Vue",
+        cualidad: [
+            { nombre: "c1", estado:false},
+            { nombre: "c2", estado:true},
+            { nombre: "c3", estado:false},
+        ]
+               
+    }
+})
